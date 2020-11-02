40fcfab0869e39242e68f738c46862a5e090a043
Yaqeen-stak <engyaqeenhamd@yahoo.com>
2020-11-02 17:30:36 +0200

    without css

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 'use strict';
+require('dotenv').config(); 
 const express = require('express');
 const superagent = require('superagent');
-const ejs = require('ejs');
+
 const app = express();
 const PORT = process.env.PORT || 3004;
 const cors = require('cors');
-require('dotenv').config(); 
-const superagent = require('superagent');
+
+
+
 
 app.set('view engine', 'ejs');
-app.use(express. urlencoded({extended:true }));
+app.use(express. urlencoded({extended:true })); // to get values from form by requse them.
 app.use(cors());
 app.use(express.static('public/styles'));
-app
+
 app.get('/', welcomePage);
 
 app.get('/hello',firstPage);
 app.get('/searches/new',secondPage);
-app.get('/searches',searchBook);
-app.use('*', notFound);
+app.post('/searches',searchBook);
+// app.use('*', notFound);
 
-const Book-Key=process.env.Book-Key;
+const Book_Key =process.env.Book_Key;
 function firstPage(request, response){
     // const objRes={
     //     Title : 'Hello word'
@@ -36,34 +38,50 @@ function welcomePage(request, response) {
     response.status(200).send('Home Page Welcome to express');
 };
 function searchBook(request, response){
+    let title = request.body.title;
+    let type = request.body.search;
+
+    let urlbooks= '';
+    if (type === 'title')
+    // booktype = 'intitle';
+    url = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+intitle`;
+
+    else 
+    // booktype = 'inauthour';
+    url = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+inauthor`;
+
+
+    // const url = `https://www.googleapis.com/books/v1/volumes?q=${booktype}+${title}`;
+   
+    // const parametrs={
+    //     key: Book_Key,
+    //     q: `${booktype}+${title}`
+    //     maxResults: 10
+       
+    // }
+    // let books=[];
    superagent.get(url).then(data =>{
-       let result = data.body.items.map(element=>{
-           return new Book(element);
+      let result= data.body.items.map(element=>{
+         return new Book(element);
        });
-       response.render('pages/searches/new',)
+       response.render('pages/searches/new',{booksResult:result})
    })
+//    .catch(()=>response.render('pages/error',{error:'No Results'})
+//    )
 }
 
-function notFound(request, response) {
-    response.status(404).send('Error');
-};
+// function notFound(request, response) {
+//     response.status(404).send('Error');
+// };
 
 function Book(bookData){
-    this.title=bookData.title;
-    this.author=bookData.author;
-    this.imgUrl=bookData.imgUrl || 'https://i.imgur.com/J5LVHEL.jpg' ;
-
-
+    this.title=bookData.volumeInfo.title || 'Book Title';
+    this.author=bookData.volumeInfo.author || 'Authour Name';
+    this.description = bookData.volumeInfo.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    this.imageLinks=bookData.volumeInfo.imageLinks || 'https://i.imgur.com/J5LVHEL.jpg';
 }
 
-
-
-
-
-
-
-
-
-
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
- 
+app.use('*', (request, resp) => {
+    resp.status(404).send('Not found!!');
+  }) 
